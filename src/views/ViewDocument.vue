416616bb383d845777aea71cbdd9dc5d9c94787b
yyjlincoln <yyjlincoln@gmail.com>
2020-08-04 13:45:39 +1000

    Update: Generate another documentAccess token for manual download.

--- src/views/ViewDocument.vue
+++ src/views/ViewDocument.vue
@@ -4,7 +4,7 @@
     <div style="padding-top: 20px;">
       <h1>{{title}}</h1>
       <p>{{subtitle}}</p>
-      <a :href="link" v-if="link">Download...</a>
+      <a @click="downloadManually" href="#" v-if="link">Download...</a>
     </div>
   </div>
 </template>
@@ -18,8 +18,33 @@ export default {
   data: () => ({
     title: "Please wait...",
     subtitle: "Initiating download...",
+    link: null
   }),
-  methods: {},
+  methods: {
+    downloadManually: function () {
+      console.log("Download")
+      this.$Global
+        .getURI("https://apis.mcsrv.icu/getDownloadLink", {
+          params: {
+            docID: this.$route.query.docID,
+            token: this.$route.query.token,
+            link: null,
+          },
+        })
+        .then((res) => {
+          if (res.data.code == 0) {
+            window.location = "https://apis.mcsrv.icu" + res.data.link;
+          } else {
+            this.subtitle = res.data.message;
+            this.title = "An error occured.";
+          }
+        })
+        .catch((e) => {
+          this.subtitle = String(e);
+          this.title = "An error occured.";
+        });
+    },
+  },
   mounted() {
     this.subtitle = "Loading document info...";
     try {
@@ -56,8 +81,8 @@ export default {
                 },
               });
             } else if (res.data.code == -501 || res.data.code == -502) {
-              this.title="Please open thie page in the browser."
-                this.subtitle = res.data.message;
+              this.title = "Please open thie page in the browser.";
+              this.subtitle = res.data.message;
             } else {
               this.title = "An error occured";
               this.subtitle =
