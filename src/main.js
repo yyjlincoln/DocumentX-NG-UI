a6c3f0de6dcd59a2f9241bfad04df9ea69aa8a30
Lincoln Yan <yyjlincoln@gmail.com>
2020-10-24 12:10:09 +1100

    Update: Update version

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const axios = require("axios")
 
 Vue.prototype.$Global = {
   config: {
-    version: 'build 20201019-1',
+    version: 'build 2020102401',
     debug: false,
   },
   toggleDebug: function () {
