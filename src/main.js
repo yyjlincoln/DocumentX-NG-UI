ea5b6288c20c2cb1648594f7492904af42cc8ad6
yyjlincoln <yyjlincoln@gmail.com>
2021-05-31 03:23:05 +0000

    Revert Commits

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
 // import md5 from 'md5'
 
 const sha256 = require("js-sha256").sha256
